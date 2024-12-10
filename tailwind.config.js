c8bfb261c0ac44aae97c29dfca6d42d367c052dc
shubham-sangale-11 <shubhamsap777@gmail.com>
2024-12-10 23:07:58 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
+
+// okay this is done
